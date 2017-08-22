48709b704d60acc863a279e6819472fc9ec5b02b
Mayank Joshi <mayank@aisle.co>
2017-08-23 02:15:11 +0530

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 // create app
-app = express();
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+    if (req.headers['x-forwarded-proto'] == 'http') {
+        next();
+    } else{
+        res.redirect('https://' + req.hostname + req.url)
+    }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("express server is up and running.");
 });
